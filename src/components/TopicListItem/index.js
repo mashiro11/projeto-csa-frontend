dc0b205078ae49aee9044d6f8f6fc6d7915b7efd
thiago <thiagoaas1990@gmail.com>
2020-02-23 09:34:02 -0300

    Showing topic latest update

--- src/components/TopicListItem/index.js
+++ src/components/TopicListItem/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const styles = {
-
+  latestUpdate:{
+    display: 'flex',
+    flexDirection: 'column'
+  },
   hr:{
     backgroundColor: '#EEEEEE',
     color: '#EEEEEE',
@@ -11,9 +14,17 @@ const styles = {
 }
 
 const TopicListItem = ({topic}) => {
+
   return(
     <div>
       <Link to={`conversas/${topic.id}`}>{topic.name}</Link>
+      {topic.messages[0]?
+        <div>
+          <div>{topic.messages.length}</div>
+          <div>Último em</div>
+          <div>{topic.messages[topic.messages.length-1].createdAt}</div>
+        </div>
+      :null}
       <hr style={styles.hr}/>
     </div>
   )
