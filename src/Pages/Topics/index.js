9e5f21f12f4ccf9acf2105a46385701c271b0e8f
thiago <thiagoaas1990@gmail.com>
2020-02-25 23:30:50 -0300

    Topic list css

--- src/Pages/Topics/index.js
+++ src/Pages/Topics/index.js
@@ -4,7 +4,35 @@ import request from '../../request.js'
 
 import TopicListItem from '../../components/TopicListItem'
 
+const styles = {
+  filters:{
+    cursor: 'pointer',
+    color: '#009688',
+    backgroundColor: '#EEEEEE',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 10
+  },
+  filterQuant:{
+    display: 'inline-block',
+    color: '#FFFFFF',
+    fontSize: 12,
+    backgroundColor: '#009688',
+    paddingRight: 5,
+    paddingLeft: 5,
+    borderRadius: '50%',
+    fontWeight: 'bold'
+  },
+  header:{
+    display: 'flex',
+    color: '#DF8400',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    margin: 20
+  }
+}
 const Topics = () => {
+
   const [topics, setTopics] = useState([])
   const handleError = (error) => {
     console.log('error:', error)
@@ -12,20 +40,26 @@ const Topics = () => {
 
   useEffect( () => request('topics', setTopics, handleError), [])
 
-
   return (
     <div>
-      <div>Filtros</div>
+      <div style={styles.filters}>
+        <span>Filtros</span>
+        <span> </span>
+        <span style={styles.filterQuant}>5</span>
+      </div>
 
       <div>
-        <div>Cabeçalho</div>
+        <div style={styles.header}>
+          <div>TEMAS</div>
+          <div>COMENTÁRIOS</div>
+        </div>
         {topics.length === 0 ?
           <div>Fetching data...</div>
         : topics.map((item, index) =>
           <TopicListItem topic={item} key={index}/>
         )}
       </div>
-      
+
       <div>Nova Conversa</div>
     </div>
   )
--- src/components/TopicListItem/index.js
+++ src/components/TopicListItem/index.js
@@ -2,29 +2,42 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const styles = {
+  container:{
+    position:'relative',
+    display:'flex',
+    flexDirection: 'horizontal'
+  },
+  responses:{
+    color: '#000000'
+  },
   latestUpdate:{
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    textAlign: 'center',
+    color: '#979797',
   },
   hr:{
     backgroundColor: '#EEEEEE',
     color: '#EEEEEE',
     borderColor: '#FFFFFF'
-  }
+  },
 }
 
 const TopicListItem = ({topic}) => {
-
+  const formatDate = (dateStringFormat) => dateStringFormat.substring(0, dateStringFormat.indexOf('T'))
   return(
     <div>
-      <Link to={`conversas/${topic.id}`}>{topic.name}</Link>
-      {topic.messages[0]?
-        <div>
-          <div>{topic.messages.length}</div>
-          <div>Último em</div>
-          <div>{topic.messages[topic.messages.length-1].createdAt}</div>
-        </div>
-      :null}
+      <div style={styles.container}>
+        <Link to={`conversas/${topic.id}`}
+          style={styles.link}>{topic.name}</Link>
+        {topic.messages[0]?
+          <div style={styles.latestUpdate}>
+            <div style={styles.responses}>{topic.messages.length}</div>
+            <div>Último em</div>
+            <div>{formatDate(topic.messages[topic.messages.length-1].createdAt)}</div>
+          </div>
+        :null}
+      </div>
       <hr style={styles.hr}/>
     </div>
   )
