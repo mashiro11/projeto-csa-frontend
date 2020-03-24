6ea585b3b3e9db66f8c51673f27b3d41302a4924
thiago <thiagoaas1990@gmail.com>
2020-03-24 10:06:25 -0300

    Date display format fixed

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
 const formatDate = (dateStringFormat, showHour) => {
   let date = dateStringFormat
     .substring(0, dateStringFormat.indexOf('T'))
-    .replace(/-/g,'/')
+    .split('-')
+  date = date[2] + "/" + date[1] + "/" + date[0]
 
   if(showHour){
     let time = dateStringFormat
