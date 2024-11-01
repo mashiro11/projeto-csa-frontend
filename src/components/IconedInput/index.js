9381cace601f924bbce1f4cbdac3f004c0842528
thiago <thiagoaas1990@gmail.com>
2024-11-01 19:55:38 -0300

    using inputAdornment

--- src/components/IconedInput/index.js
+++ src/components/IconedInput/index.js
@@ -2,19 +2,30 @@ import React from 'react'
 import EmailIcon from '@material-ui/icons/Email.js'
 import LockIcon from '@material-ui/icons/Lock.js'
 import TextField from '@material-ui/core/TextField'
+import InputAdornment  from '@material-ui/core/InputAdornment'
 
 const IconedInput = ({onChange, placeholder, value, type, icon}) => {
+    const displayedIcon = icon === "Email" ?
+                            <EmailIcon color={"disabled"}/>
+                            : icon === "Lock"?
+                            <LockIcon color={"disabled"}/>
+                        : null;
+    
     return (
-        <div>
-            {icon === "Email" ?
-                <EmailIcon color={"disabled"}/>
-                : icon === "Lock"?
-                <LockIcon color={"disabled"}/>
-            : null
-            }
-            <TextField variant="outlined"/>
-          <input placeholder={placeholder} type={type} value={value} onChange={onChange}/>
-        </div>
+        <TextField
+            id="input-with-icon-adornment"
+            variant="outlined"
+            placeholder={placeholder} type={type} value={value} onChange={onChange}
+            slotProps={{
+                input: {
+                    startAdornment: (
+                    <InputAdornment position="start">
+                        {displayedIcon}
+                    </InputAdornment>
+                    )
+                }
+            }}
+        />
     )
 }
 
